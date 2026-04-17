83fa2342bcc83b11d54b4dbe4180e05be7370b0e
Polupoltinnyikh.MM <polupoltinnyikh.mm@dns-shop.ru>
2026-04-17 23:24:24 +1000

    Refactor: define_query()

--- src/dbhose_airflow/common/text_io.py
+++ src/dbhose_airflow/common/text_io.py
@@ -1,9 +1,16 @@
 from pathlib import Path
 
+from .errors import DBHoseAirflowValueError
+from .structs import (
+    DQCheck,
+    MoveMethod,
+)
+
 
 __root_path = Path(__file__).parent.absolute()
 __logo_path = __root_path / "renders" / "LOGO"
 __query_path = __root_path / "queries" / "{}" / "{}" / "{}.sql"
+__query_type = {DQCheck: "dq", MoveMethod: "move"}
 
 
 def __read_text(path: str | Path) -> str:
@@ -13,10 +20,15 @@ def __read_text(path: str | Path) -> str:
         return file.read()
 
 
-def define_query(query_type: str, dbname: str, kind: str) -> str:
+def define_query(dbname: str, kind: DQCheck | MoveMethod) -> str:
     """Define specify query."""
 
-    return __read_text(str(__query_path).format(query_type, dbname, kind))
+    try:
+        query_type = __query_type[type(kind)]
+        path = str(__query_path).format(query_type, dbname, kind.name)
+        return __read_text(path)
+    except (KeyError, ValueError, TypeError) as error:
+        raise DBHoseAirflowValueError(error)
 
 
 def logo() -> str:
